485012da9f679f18f059b8c06e2cc016b7d23ee1
danilmunoz1996 <danilmunoz@inf.udec.cl>
2019-06-21 06:24:36 -0400

    aguegue la info sala

---
+++ app/src/main/java/com/example/iu/Activities/ExpLVAdapter.java
@@ -0,0 +1,99 @@
+package com.example.iu.Activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+
+import com.example.iu.R;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class ExpLVAdapter extends BaseExpandableListAdapter {
+
+    private ArrayList<String> listCategoria;
+    private Map<String, ArrayList<String>> mapChild;
+    private Context context;
+
+    public ExpLVAdapter(ArrayList<String> listCategoria, Map<String, ArrayList<String>> mapChild, Context context) {
+        this.listCategoria = listCategoria;
+        this.mapChild = mapChild;
+        this.context = context;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listCategoria.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return mapChild.get(listCategoria.get(i)).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return listCategoria.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return mapChild.get(listCategoria.get(i)).get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        String tituloCategoria = (String) getGroup(i);
+        View convertView = LayoutInflater.from(context).inflate(R.layout.elv_group, null);
+        TextView tvGroup = (TextView) convertView.findViewById(R.id.tvGroup);
+        tvGroup.setText(tituloCategoria);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        System.out.println("wena");
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.elv_child, null);
+        }
+
+        String item = (String) getChild(i, i1);
+        System.out.println(item);
+        TextView textView = (TextView) view.findViewById(R.id.tvChild);
+        textView.setText(item);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,info_sala.class);
+                context.startActivity(intent);
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return true;
+    }
+}
+
