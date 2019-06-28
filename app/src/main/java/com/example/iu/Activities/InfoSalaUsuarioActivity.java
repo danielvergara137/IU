592ce7f9c53db6b59eff49b2c0cd407469571eb0
danilmunoz1996 <danilmunoz@inf.udec.cl>
2019-06-27 22:56:46 -0400

    aguegue lo del horario

--- app/src/main/java/com/example/iu/Activities/InfoSalaUsuarioActivity.java
+++ app/src/main/java/com/example/iu/Activities/InfoSalaUsuarioActivity.java
@@ -19,15 +19,14 @@ public class InfoSalaUsuarioActivity extends AppCompatActivity {
     private String nombre;
     private String horario;
     private TextView vistanombre;
-    private TextView vistafacultad;
     private TextView vistacapacidad;
     private TextView vistaramo;
     private TextView vistaprofe;
     private TextView vistacorreo;
     private LinearLayout reserva;
-    private Button boton19;
-    private Button boton20;
-    private Button boton21;
+    private Button b10;
+    private Button b20;
+    private Button b30;
 
 
     @Override
@@ -40,34 +39,32 @@ public class InfoSalaUsuarioActivity extends AppCompatActivity {
         sala = DBQueries.getSala(nombre,this);
         horario = sala.getHorario();
         vistanombre = (TextView)findViewById(R.id.InfoSalaUsuarioActivity_sala);
-        vistafacultad = (TextView)findViewById(R.id.InfoSalaUsuarioActivity_facultad);
         vistacapacidad = (TextView)findViewById(R.id.InfoSalaUsuarioActivity_capacidad);
         vistaramo = (TextView)findViewById(R.id.InfoSalaUsuarioActivity_ramo);
         vistaprofe = (TextView)findViewById(R.id.InfoSalaUsuarioActivity_profe);
         vistaramo.setVisibility(View.INVISIBLE);
         vistaprofe.setVisibility(View.INVISIBLE);
-        boton19 = (Button)findViewById(R.id.button19);
-        boton20 = (Button)findViewById(R.id.button20);
-        boton21 = (Button)findViewById(R.id.button21);
+        b10 = (Button)findViewById(R.id.b10);
+        b20 = (Button)findViewById(R.id.b20);
+        b30 = (Button)findViewById(R.id.b30);
 
-        vistanombre.setText(sala.getNombre());
-        vistafacultad.setText(sala.getFacultad());
-        vistacapacidad.setText(String.valueOf(sala.getCapacidad()));
+        vistanombre.setText("Sala: " + sala.getNombre());
+        vistacapacidad.setText("Capacidad: " + String.valueOf(sala.getCapacidad()));
 
         if(horario.charAt(0)=='1') {
-            boton19.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
+            b10.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
         }
         if(horario.charAt(1)=='1') {
-            boton20.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
+            b20.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
         }
         if(horario.charAt(2)=='1') {
-            boton21.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
+            b30.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
         }
 
     }
 
     public void mostrarInfo(View view){
-        if(view==boton19) {
+        if(view==b10) {
             Reserva reserva = DBQueries.getReservaInfo(nombre, 1, this);
             vistaramo.setText(reserva.getRamo());
             vistaprofe.setText(reserva.getDocente());
